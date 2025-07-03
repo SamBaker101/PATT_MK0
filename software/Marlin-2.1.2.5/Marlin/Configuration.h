31f63b88c61556b99e2376a2ff6325d02240d764
Sam Baker <Sam.Baker101@gmail.com>
2025-07-03 16:08:16 -0400

    Reduce max feedrate and acceleration until things are more stable

--- software/Marlin-2.1.2.5/Marlin/Configuration.h
+++ software/Marlin-2.1.2.5/Marlin/Configuration.h
@@ -1202,7 +1202,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 50, 50, 80, 80 }
+#define DEFAULT_MAX_FEEDRATE          { 25, 25, 20, 80 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -1215,7 +1215,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 700, 600, 50, 30000 }
+#define DEFAULT_MAX_ACCELERATION      { 400, 400, 30, 30000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
